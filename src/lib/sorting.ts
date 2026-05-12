387b957f8074836c9bb855204593676ea98b34fa
Marc Pop <marcpop2003@yahoo.com>
2026-05-12 20:13:20 +0300

    Bugfix 2: sortByDate does not mutate array

--- src/lib/sorting.ts
+++ src/lib/sorting.ts
@@ -4,7 +4,7 @@ import { Invoice } from "../types"
  * Returns invoices sorted chronologically by date, oldest first.
  */
 export function sortByDate(invoices: Invoice[]): Invoice[] {
-  return invoices.sort((a, b) => {
+  return [...invoices].sort((a, b) => {
     return new Date(a.date).getTime() - new Date(b.date).getTime()
   })
 }
